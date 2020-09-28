a39312b6e93cddef3b3d31f2c4ee74e7e62fe618
Your Name <futunplay@gmail.com>
2020-09-28 18:46:43 +0300

    Добавлена возможность добавления таймера

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <HelloWorld v-for="n in count" :key="n"></HelloWorld>
+    <div class="app__addTimer">
+      <button class="app__addTimer-button" v-on:click="addTimer">+</button>
+    </div>
   </div>
 </template>
 
@@ -9,19 +12,58 @@ import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
+
+  data: function dsta() {
+    return {
+      count: 1,
+    };
+  },
+
+  methods: {
+    addTimer: function addTimer() {
+      this.count += 1;
+    },
+  },
+
   components: {
     HelloWorld,
   },
+
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-wrap: wrap;
+
+  & > * {
+    margin: 20px;
+  }
+}
+
+.app {
+  &__addTimer {
+    min-width: 225px;
+    width: 225px;
+    min-height: 120px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #696969;
+  }
+
+  &__addTimer-button {
+    border: none;
+    background: transparent;
+    font-size: 30px;
+    color: #9e9e9e;
+    outline: none;
+
+    &:hover,
+    &:focus{
+      color: #FFFFFF;
+    }
+  }
 }
 </style>
